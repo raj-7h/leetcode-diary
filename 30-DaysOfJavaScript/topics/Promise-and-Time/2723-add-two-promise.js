61b3b3167d74c24e393b900f02d87f0efe79de39
Raj Jha <rajjha.study@gmail.com>
2025-04-27 17:44:40 +0530

    Solve LeetCode problem #2723

--- 30-DaysOfJavaScript/topics/Promise-and-Time/2723-add-two-promise.js
+++ 30-DaysOfJavaScript/topics/Promise-and-Time/2723-add-two-promise.js
@@ -1,15 +1,30 @@
 /**
- * ✅ Problem Name: [Replace with Problem Name]
- * ✅ LeetCode Problem Number: [Replace with Problem Number]
- * ✅ Difficulty: [Easy | Medium | Hard]
- * ✅ Date Solved: [YYYY-MM-DD]
+ * ✅ Problem Name: [Add Two Promise]
+ * ✅ LeetCode Problem Number: [2723]
+ * ✅ Difficulty: [Easy]
+ * ✅ Date Solved: [2025-04-27]
  *
  * 🧠 Description:
- * [Write a short summary or copy the description from LeetCode]
+ * [Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. 
+ * The returned promise should resolve with the sum of the two numbers.]
  *
  * 🧪 Examples:
- * Input:
- * Output: 
+ * 
+ Example 1:
+
+Input: 
+promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20)), 
+promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
+Output: 7
+Explanation: The two input promises resolve with the values of 2 and 5 respectively. The returned promise should resolve with a value of 2 + 5 = 7. The time the returned promise resolves is not judged for this problem.
+
+Example 2:
+
+Input: 
+promise1 = new Promise(resolve => setTimeout(() => resolve(10), 50)), 
+promise2 = new Promise(resolve => setTimeout(() => resolve(-12), 30))
+Output: -2
+Explanation: The two input promises resolve with the values of 10 and -12 respectively. The returned promise should resolve with a value of 10 + -12 = -2.
  *
  * 💡 Approach:
  * [
