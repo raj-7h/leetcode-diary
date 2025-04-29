c87391523f9d21fed0d1bb240739a9cbf5d7a283
Raj Jha <rajjha.study@gmail.com>
2025-04-29 23:22:26 +0530

    Solve Leetcode problem #2725

---
+++ 30-DaysOfJavaScript/topics/Promise-and-Time/2725-interval-cancellation.js
@@ -0,0 +1,116 @@
+/**
+ * ✅ Problem Name: [Interval Cancellation]
+ * ✅ LeetCode Problem Number: [2725]
+ * ✅ Difficulty: [Easy]
+ * ✅ Date Solved: [2025-04-29]
+ *
+ * 🧠 Description:
+ * [Given a function fn, an array of arguments args, and an interval time t, return a cancel function cancelFn.
+
+After a delay of cancelTimeMs, the returned cancel function cancelFn will be invoked.]
+ *
+ * 🧪 Examples:
+ 
+ Example 1:
+
+Input: fn = (x) => x * 2, args = [4], t = 35
+Output: 
+[
+   {"time": 0, "returned": 8},
+   {"time": 35, "returned": 8},
+   {"time": 70, "returned": 8},
+   {"time": 105, "returned": 8},
+   {"time": 140, "returned": 8},
+   {"time": 175, "returned": 8}
+]
+Explanation: 
+const cancelTimeMs = 190;
+const cancelFn = cancellable((x) => x * 2, [4], 35);
+setTimeout(cancelFn, cancelTimeMs);
+
+Every 35ms, fn(4) is called. Until t=190ms, then it is cancelled.
+1st fn call is at 0ms. fn(4) returns 8.
+2nd fn call is at 35ms. fn(4) returns 8.
+3rd fn call is at 70ms. fn(4) returns 8.
+4th fn call is at 105ms. fn(4) returns 8.
+5th fn call is at 140ms. fn(4) returns 8.
+6th fn call is at 175ms. fn(4) returns 8.
+Cancelled at 190ms
+Example 2:
+
+Input: fn = (x1, x2) => (x1 * x2), args = [2, 5], t = 30
+Output: 
+[
+   {"time": 0, "returned": 10},
+   {"time": 30, "returned": 10},
+   {"time": 60, "returned": 10},
+   {"time": 90, "returned": 10},
+   {"time": 120, "returned": 10},
+   {"time": 150, "returned": 10}
+]
+Explanation: 
+const cancelTimeMs = 165; 
+const cancelFn = cancellable((x1, x2) => (x1 * x2), [2, 5], 30) 
+setTimeout(cancelFn, cancelTimeMs)
+
+Every 30ms, fn(2, 5) is called. Until t=165ms, then it is cancelled.
+1st fn call is at 0ms 
+2nd fn call is at 30ms 
+3rd fn call is at 60ms 
+4th fn call is at 90ms 
+5th fn call is at 120ms 
+6th fn call is at 150ms
+Cancelled at 165ms
+ *
+ * 💡 Approach:
+ * [
+*	• Call the `fn` function immediately with provided arguments.
+* 	• Use `setInterval` to repeatedly call `fn` every `t` milliseconds.
+* 	• Store the interval in a variable to cancel it later.
+* 	• Return a function that clears the interval when called.
+  ]
+ *
+ */
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  fn(...args);
+  let result = setInterval(() => fn(...args), t);
+  let cancelFn = () => {
+    clearInterval(result);
+  };
+  return cancelFn;
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)
+ */
